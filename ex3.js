16fe1d46f7099c95b2ca296e89b76e6853a9fd4b
samuel <111382153+samuelcstp@users.noreply.github.com>
2026-03-26 07:52:41 -0300

    Add files via upload

--- ex3.js
+++ ex3.js
@@ -2,23 +2,21 @@ const fs = require("fs")
 
 const produtos = JSON.parse(fs.readFileSync("./produtos.json", "utf-8"));
 
-const resumir = produtos.map(({produto, valor, categoria}) => ({produto, valor, categoria}))
+const resumir = produtos.map(({produto, valor, categoria}) => ({produto, valor, categoria})) // mapeia o array para um novo array com apenas os campos desejados
 
 console.log(resumir)
 
 const totalPorCategoria = resumir
     .reduce((acc, {categoria, valor}) => 
-        {acc[categoria] = (acc[categoria] || 0 ) + valor;
+        {acc[categoria] = (acc[categoria] || 0 ) + valor; // soma os valores por categoria ou cria a categoria se não existir
             return acc;
         }, {});
 
 console.log(totalPorCategoria)
 
-const listaPorCategoria = Object.entries(totalPorCategoria)
-    .map(([categoria, total]) => ({categoria, total}));
-
-const ordenarPorValor = listaPorCategoria
-        .toSorted((a, b) => b.total - a.total);
+const ordenarPorValor = Object.entries(totalPorCategoria)
+    .map(([categoria, total]) => ({categoria, total})) // transforma o objeto em array
+    .toSorted((a, b) => b.total - a.total); //ordena o array por valor
 
 console.log(ordenarPorValor)
 
--- ex4.js
+++ ex4.js
@@ -1,3 +1,7 @@
+const fs = require("fs")
+
+const produtos = JSON.parse(fs.readFileSync("./produtos.json", "utf-8"));
+
 const vendas = [
   { item: 'Notebook', preco: 1000, categoria: 'tech' },
   { item: 'Cadeira', preco: 150, categoria: 'moveis' },
@@ -7,6 +11,8 @@ const vendas = [
 
 const vendasVazio = []
 
+
+
 const filtrarPorValorMinimo = (min) =>
 {
 	return (vendas) => vendas.filter(item => item.preco >= min);
@@ -15,20 +21,44 @@ const filtrarPorValorMinimo = (min) =>
 const  filtrarPorCategoria = (categoria) =>
 {
 	return (vendas) => vendas.filter(item => item.categoria === categoria);
-} 
+}
 
-console.assert( 
-  filtrarPorValorMinimo(1000)(vendas).length === 2, "2 itens maior que 1000"
-); 
 
-console.assert( 
-  filtrarPorValorMinimo(-1)(vendas).length === 4, "quatro valores maior que -1"
-);
 
-console.assert(
-    filtrarPorValorMinimo(100)(vendasVazio).length === 4, "quatro valores maior que 100"
-)
+const resumir = produtos.map(({produto, valor, categoria}) => ({produto, valor, categoria}))
 
-console.assert(
-    filtrarPorCategoria('perfiféricos')(vendas).length === 1, "um valor da categoria periféricos"
-)
+const totalPorCategoria = resumir
+    .reduce((acc, {categoria, valor}) => 
+        {acc[categoria] = (acc[categoria] || 0 ) + valor;
+    return acc;
+  }, {});
+
+const ordenarPorValor = Object.entries(totalPorCategoria)
+    .map(([categoria, total]) => ({categoria, total}))
+    .toSorted((a, b) => b.total - a.total);
+
+
+//TESTES COM ASSERT
+console.assert(filtrarPorValorMinimo(800)(vendas).length === 2, "2 itens maior que 800")
+console.assert(filtrarPorValorMinimo(-1)(vendas).length === 4, "quatro valores maiores que -1")
+console.assert(filtrarPorValorMinimo(100)(vendasVazio).length === 4, "quatro valores maiores que 100 da lista vazia")
+
+console.assert(filtrarPorCategoria('perfiféricos')(vendas).length === 1, "um valor da categoria periféricos")
+console.assert(filtrarPorCategoria('moveis')(vendas).length === 1, "um valor da categoria moveis")
+console.assert(filtrarPorCategoria('moveis')(vendasVazio).length === 1, "um valor da categoria moveis da lista vazia")
+
+
+
+console.assert(resumir.length === 50, "resumir deve ter 50 itens");
+console.assert(resumir[0].vendedor === undefined, "resumir não deve conter vendedor");
+console.assert(resumir[0].produto !== undefined, "resumir deve conter produto");
+console.assert(resumir.length === 10, "resumir não tem 10 itens, tem 50");
+console.assert(resumir[0].vendedor !== undefined, "resumir não contém vendedor");
+
+console.assert(totalPorCategoria['tech'] === 7700, "total de tech deve ser 7700");
+console.assert(totalPorCategoria['periféricos'] === 1000, "total de periféricos deve ser 1000");
+console.assert(totalPorCategoria['tech'] === 0, "tech não soma 0");
+console.assert(totalPorCategoria['inexistente'] === 500, "categoria inexistente não existe");
+
+console.assert(ordenarPorValor[0].total >= ordenarPorValor[1].total, "deve estar em ordem decrescente");
+console.assert(ordenarPorValor[0].total <= ordenarPorValor[1].total, "primeiro item tem total MAIOR que o segundo");
